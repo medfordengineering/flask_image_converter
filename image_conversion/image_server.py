63780e6587515f55e786bcec7bea04584ba89039
Sam Christy <schristy@medford.k12.ma.us>
2020-04-28 07:09:15 -0400

    Added ability to change the quality of the image with a slider

--- image_conversion/image_server.py
+++ image_conversion/image_server.py
@@ -15,11 +15,21 @@ def uploaded_file():
 		outfile = infile.split('.')[0] + '.pbm'
 		viewfile = infile.split('.')[0] + '.gif'
 		f.save('static/' + infile)
-		command = 'convert {} -resize 128x64 -dither FloydSteinberg -remap pattern:gray50 -compress none {}'.format('static/' + infile, 'static/' + outfile) 
+		size = request.form.get('size')
+		dots = request.form.get('dots')
+		command = 'convert {} +level {}%,100% -resize {} -dither FloydSteinberg -remap pattern:gray50 -compress none {}'.format('static/' + infile, dots, size, 'static/' + outfile) 
 		os.system(command)
 		command = 'convert {} {}'.format('static/' + outfile, 'static/' + viewfile)
 		os.system(command)
 		return render_template('uploaded.html', user_image = viewfile)
-		
+
+@app.after_request
+def add_header(r):
+	r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	r.headers["Pragma"] = "no-cache"
+	r.headers["Expires"] = "0"
+	r.headers['Cache-Control'] = 'public, max-age=0'
+	return r	
+
 if __name__ == '__main__':
 	app.run(debug = True)
